2c69d8be13505fac02c8fa2e449beb66331b8b13
root <damvuong.work@gmail.com>
2024-11-19 20:50:17 +0900

    setup eureka and docker replica

--- spring-cloud-todo-list-service/src/main/java/com/spring/cloud/controller/TodoListController.java
+++ spring-cloud-todo-list-service/src/main/java/com/spring/cloud/controller/TodoListController.java
@@ -4,11 +4,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @RequestMapping("/v1/todo-list-service")
 public class TodoListController {
     @GetMapping("/hello-world")
     public String helloWorld() {
-        return "Todo list service, Hello World!";
+        return "Todo list service, Hello World! with IP: " + getHostIp();
+    }
+
+    private String getHostIp() {
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            return inetAddress.getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            return "Unable to fetch IP";
+        }
     }
 }
--- spring-cloud-user-service/src/main/java/com/spring/cloud/controller/UserController.java
+++ spring-cloud-user-service/src/main/java/com/spring/cloud/controller/UserController.java
@@ -4,11 +4,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @RequestMapping("/v1/user-service")
 public class UserController {
     @GetMapping("/hello-world")
     public String helloWorld() {
-        return "User service, Hello World!";
+        return "User service, Hello World! with IP: " + getHostIp();
+    }
+
+    private String getHostIp() {
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            return inetAddress.getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            return "Unable to fetch IP";
+        }
     }
 }
